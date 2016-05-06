104198a5ac6ef651d2a6bcadc7752a5cb852ee58
Anthony Mousoulis <anthony.mousoulis2015@hotmail.com>
2016-05-05 23:25:43 -0400

    Commit with errors!!!

--- BasicCsharp/Program.cs
+++ BasicCsharp/Program.cs
@@ -8,8 +8,28 @@ namespace BasicCsharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // fuction containing access containers and instructions...
         {
+            /* Following the procedures with the class instructions */
+
+            // namespaces - while executing find contents in libraries we are "" Using"
+            Console.WriteLine("Hello world ...");
+
+            Console.ReadLine(); //same as .Read or pause
+
+
+            System.Console.WriteLine("Hello World Again!!!\n");
+            Console.WriteLine("Please tell me your first name: ");
+
+            string firstName;
+            //firstName = Console.OpenStandardInput();
+
+            string lastName = "";
+            //Console.WriteLine("\nNow " + firstName + " please tell me your last name: ");
+            //lastName = Console.Read();
+
+            //System.Console.pause
+            //var config = CommandType
         }
     }
 }
